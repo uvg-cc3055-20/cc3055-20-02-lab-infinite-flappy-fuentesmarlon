b401dc14a2eccab803b5c41016d1f23257c0d193
marlord <marlon_dx@hotmail.com>
2018-01-27 14:21:38 -0600

    Version 2- HASTA 2.9

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-
+    bool gameOver = false;
+    int score = 0;
+    public static GameController instance; 
 	// Use this for initialization
 	void Start () {
-		
+        instance = this;
 	}
 	
 	// Update is called once per frame
